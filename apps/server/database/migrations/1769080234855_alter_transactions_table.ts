a0d788613d8b6b39b224ddc1acde833f98fbab93
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2026-01-22 19:23:25 +0800

    added recordedAt on transaction table

---
+++ apps/server/database/migrations/1769080234855_alter_transactions_table.ts
@@ -0,0 +1,17 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'transactions'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.timestamp('recorded_at', { useTz: true }).nullable()
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('recorded_at')
+    })
+  }
+}
