e5e3193eeddb9b00f98e121829ff796aa10478e7
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2026-04-29 17:15:24 +0800

    fix callback endpoint

--- apps/web/src/lib/request.ts
+++ apps/web/src/lib/request.ts
@@ -2,50 +2,58 @@ import { type FetchOptions, ofetch } from "ofetch"
 import { getAuthToken } from "./auth-token"
 import { camelKeys } from "./utils"
 
+const isDev = import.meta.env.DEV
+
 const ofetchBaseConfig: FetchOptions = {
 	timeout: 3000,
 	async onRequest({ request, options }) {
-		console.log("[fetch request]", request, options)
+		if (isDev) {
+			console.log("[fetch request]", request, options)
+		}
 	},
 	async onRequestError({ request, response, error }) {
-		console.log(
-			"[fetch request error]",
-			request,
-			response?.status,
-			response?.body,
-		)
-		console.error(error)
+		if (isDev) {
+			console.log(
+				"[fetch request error]",
+				request,
+				response?.status,
+				response?.body,
+			)
+			console.error(error)
+		}
 	},
 	async onResponse({ request, options, response }) {
-		console.log("[fetch response]", request, options)
+		if (isDev) {
+			console.log("[fetch response]", request, options)
+		}
 		response._data = camelKeys(response._data)
 	},
 }
 
-const isProd = import.meta.env.PROD
-
 export const request = ofetch.create({
 	...ofetchBaseConfig,
-	baseURL: isProd
-		? ""
-		: import.meta.env.VITE_API_URL || "http://localhost:3333",
+	baseURL: import.meta.env.VITE_API_URL || "",
 })
 
 export const requestApi = ofetch.create({
 	...ofetchBaseConfig,
-	baseURL: isProd
-		? "/api"
-		: `${import.meta.env.VITE_API_URL || "http://localhost:3333"}/api`,
+	baseURL: import.meta.env.VITE_API_URL
+		? `${import.meta.env.VITE_API_URL}/api`
+		: "/api",
 	credentials: "include",
 	async onRequest({ request, options }) {
-		console.log("[fetch request]", request, options)
+		if (isDev) {
+			console.log("[fetch request]", request, options)
+		}
 		const token = getAuthToken()
 		if (token) {
 			options.headers.set("Authorization", `Bearer ${token}`)
 		}
 	},
 	async onResponse({ request, options, response }) {
-		console.log("[fetch response]", request, options)
+		if (isDev) {
+			console.log("[fetch response]", request, options)
+		}
 		response._data = camelKeys(response._data)
 		if (response.status === 401) {
 			console.warn("Unauthorized: redirecting to login")
