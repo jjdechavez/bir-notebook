4d4975503b7e34181c5e6faa9bb7eb5a6d49dc21
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2026-04-29 15:40:57 +0800

    fix creating user

--- apps/server/db/migrations/20260323T125554_seeder_accounts.mjs
+++ apps/server/db/migrations/20260323T125554_seeder_accounts.mjs
@@ -2,6 +2,7 @@ import { fileURLToPath } from "node:url"
 import { Kysely } from "kysely"
 import { loadAppConfig } from "../../config.js"
 import { getAuth } from "../auth.js"
+import { getPool } from "../index.js"
 
 /**
  * @param {Kysely<any>} db
@@ -18,24 +19,27 @@ export async function up(db) {
 		const config = await loadAppConfig(
 			fileURLToPath(new URL(".", import.meta.url)),
 		)
-		const auth = getAuth(config)
+		const pool = getPool(config)
+		const auth = getAuth(config, pool)
 
 		const firstName = "Admin"
 		const lastName = "Acme"
 		const name = `${firstName} ${lastName}`
-		await auth.api.createUser({
-			body: {
-				email: adminEmail,
-				name: name,
-				role: "admin",
-				password: "admin@Acme",
-				emailVerified: true,
-				data: {
-					firstName,
-					lastName,
+		await auth.api
+			.createUser({
+				body: {
+					email: adminEmail,
+					name: name,
+					role: "admin",
+					password: "admin@Acme",
+					emailVerified: true,
+					data: {
+						firstName,
+						lastName,
+					},
 				},
-			},
-		})
+			})
+			.catch((e) => console.error(e))
 	}
 }
 
