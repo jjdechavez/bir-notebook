f2209dd1b27ee613652a50b3fc7a527ef531fbc4
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2026-05-19 16:04:11 +0800

    updated dark mode of highlight'

--- apps/web/src/components/books/transfered-gl-badge.tsx
+++ apps/web/src/components/books/transfered-gl-badge.tsx
@@ -2,7 +2,7 @@ import { ArrowRight } from "lucide-react"
 
 export function TransferedGLBadge() {
 	return (
-		<span className="text-sm bg-highlight text-highlight-foreground flex items-center gap-x-1 px-2 rounded">
+		<span className="text-sm font-medium bg-highlight text-highlight-foreground flex items-center gap-x-1 px-2 rounded">
 			<ArrowRight className="w-3 h-3" />
 			GL
 		</span>
